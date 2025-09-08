72ff784bd33133c42ddff3a9a26fb948f21bf53f
NasserGh93 <nasserghaibeh@googlemail.com>
2025-09-08 09:55:27 +0200

    update

---
+++ debugger.js
@@ -0,0 +1,15 @@
+function init() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+}
+function testOne() {
+
+}
+function tetTwo() {
+
+}
+function debbugerOutput(text) {
+    let debbugerLogRef = document.getElementById('debugger_log');
+    debuggerLogRef.innerHTML = text;
+}
