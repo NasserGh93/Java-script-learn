3b517a7531c384f15b391762b48e7dd5d8e95222
NasserGh93 <nasserghaibeh@googlemail.com>
2025-09-26 22:57:57 +0200

    save

--- arrays_and_loops.js
+++ arrays_and_loops.js
@@ -1,1 +1,11 @@
-let myList = ["Apfel", "Banane", "Kirsche"];
+let myList = [12, "Banana", 1];
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+let singletestVar = fruits.pop();
+function initArrays(){
+    console.log(singletestVar);
+}
+
+containsElement(array, element){
+
+}
